2357ce0471406a6450bda912b355393f40f1d815
Anthony Sim <anthonyysim@gmail.com>
2021-02-27 19:44:28 -0800

    dependencies

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'index_bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: './client/src/index.html' })],
+};
