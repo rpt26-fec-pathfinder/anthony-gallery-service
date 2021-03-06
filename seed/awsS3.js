99084d2de5b6b6a64f12c0760887c8714eae876a
Anthony Sim <anthonyysim@gmail.com>
2021-03-05 21:24:45 -0800

    aws sdk auto upload working

--- seed/awsS3.js
+++ seed/awsS3.js
@@ -1,68 +1,73 @@
-// tested doesn't work, don't use!!!
+// Don't use, I tried to mess aronud with aws sdk trying to get it to work....no bueno.
+// Only loads a single file at a time?! Tried to load multiple files, but it kept blowing up and ran out of time.
 
-// const AWS = require('aws-sdk');
-// const uuid = require('uuid');
-// const fs = require('fs');
-// const path = require('path');
-// const dotenv = require('dotenv');
-// dotenv.config({ path: 'config/config.env' });
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'config/config.env' });
 
-// // initializes aws s3
-// const s3 = new AWS.S3({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   region: 'us-east-1',
-// });
+// initializes aws s3
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-1',
+});
 
-// //  lists all your s3 buckets just to test you're connected to aws
-// s3.listBuckets((err, data) => {
-//   err ? console.error(err) : console.log(data);
-// });
+//  lists all your s3 buckets just to test you're connected to aws
+s3.listBuckets((err, data) => {
+  err ? console.error(err) : console.log(data);
+});
 
-// // creates a unique bucketname
-// const bucketName = 'steam-' + uuid.v4();
-// const params = {
-//   Bucket: bucketName,
-// };
+// creates a unique bucketname
+const bucketName = 'steam-' + uuid.v4();
+const params = {
+  Bucket: bucketName,
+};
 
-// // creates a bucket
-// s3.createBucket(params, function (err, data) {
-//   err ? console.log(err, err.stack) : console.log('Bucket created successfully:', data.Location);
-// });
+// creates a bucket
+s3.createBucket(params, function (err, data) {
+  err ? console.log(err, err.stack) : console.log('Bucket created successfully:', data.Location);
+});
 
-
-
-// // path to images folder
-// const imageDirPath = path.join(__dirname, '../images');
-// const files = fs.readdirSync(imageDirPath);
-// console.log(files);
-
-
-// // uploads the files to aws s3 bucket
-// const uploadFile = (fileName) => {
-//   // const fileContent = fs.readFileSync(fileName);
-
-//   // params set up
-//   const params = {
-//     Bucket: bucketName,
-//     Key: fileName, // file name you want to save as in S3
-//     Body: stream
-//   };
-
-//   // uploads the file
-//   s3.upload(params, function (err, data) {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log('File uploaded successfully:', data.Location);
-//   });
-// };
+const sendToAWS = (param) => {
+  return new Promise((resolve, reject) => {
+    s3.putObject(param, (err, data) => {
+      if (err) {
+        reject('Error! Image file was NOT saved!')
+      } else {
+        resolve('Success! Image file was saved!')
+      }
+    })
+  })
+}
 
 
-// uploadFile(imageDirPath + '/header-1.jpg');
+// uploads the files to aws s3 bucket
+const uploadFiles = async () => {
+  try {
+    const imageDirPath = path.join(__dirname, '../images');
+    const files = fs.readdirSync(imageDirPath);
 
+    for (let file of files) {
+      let content = fs.readFileSync(imageDirPath + `/${file}`)
 
+      let param = {
+        Bucket: bucketName,
+        ACL: 'public-read',
+        ContentType: 'image/jpeg',
+        Body: content,
+        Key: file, // file name you want to save as in S3
+      };
 
+      let result = await sendToAWS(param);
+      console.log(result)
+    }
 
+  } catch (err) {
+    console.error(err)
+  }
+};
 
-
+uploadFiles();
