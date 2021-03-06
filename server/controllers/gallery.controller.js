6c3e190249a723d1f82dfa23d074779704313ff8
Anthony Sim <anthonyysim@gmail.com>
2021-03-06 14:42:33 -0800

    refactored controller

--- server/controllers/gallery.controller.js
+++ server/controllers/gallery.controller.js
@@ -1,40 +1,19 @@
 const Model = require('../db-models/gallery.model');
 
 exports.getGallery = (req, res) => {
-  // delete this portion when there are actually 100 records worth of photos. avg 20-30ish per page.
-  if (req.params.page > 5 && req.params.page <= 100) {
-    let randomPage = Math.floor(Math.random() * (5) + 1);
-    console.log(`The random page that was generated was page ${randomPage}!`);
+  Model.find({ page: req.params.page }, (err, data) => {
+    if (err) {
+      throw err;
 
-    Model.find({ page: randomPage }, (err, data) => {
-      if (err) {
-        throw err;
-
-      } else {
-        if (data.length === 0) {
-          res.json({ message: `Page ${req.params.page} does NOT exist!` });
-
-        } else {
-          res.json(data);
-        }
-      }
-    });
-
-  } else {
-    Model.find({ page: req.params.page }, (err, data) => {
-      if (err) {
-        throw err;
+    } else {
+      if (data.length === 0) {
+        res.json({ message: `Page ${req.params.page} does NOT exist!` });
 
       } else {
-        if (data.length === 0) {
-          res.json({ message: `Page ${req.params.page} does NOT exist!` });
-
-        } else {
-          res.json(data);
-        }
+        res.json(data);
       }
-    });
-  }
+    }
+  });
 };
 
 
