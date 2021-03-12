e8a7df32f8d78ea2ee0dd890e063ff8559555321
Anthony Sim <anthonyysim@gmail.com>
2021-03-12 14:43:29 -0800

    added navbar and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Gallery from './components/Gallery.jsx';
+import NavBar from './components/NavBar.jsx';
 
 
 const App = () => {
   return (
-    <div>
+    <div id="content">
+      <NavBar />
       <Gallery />
     </div>
   );
