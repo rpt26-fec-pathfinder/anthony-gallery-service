bada2697999ed4f57c53045069d755f9a8415dda
Anthony Sim <anthonyysim@gmail.com>
2021-03-19 14:50:01 -0700

    removed navbar

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import HubButton from './HubButton.jsx';
+import Categories from './Categories.jsx';
+import SignIn from './SignIn.jsx';
 import '../styling/Gallery.css';
 import axios from 'axios';
 import StopTimer from '../helpers/stopTimer.jsx'
-// import { motion, AnimatePresence } from "framer-motion";
 
 
 const Gallery = () => {
@@ -14,11 +15,6 @@ const Gallery = () => {
     animate: true,
     main: [],
     thumb: [],
-    // title: 'Age of Empires II: Definitive Edition',
-    // idx: -1,
-    // preSelectedImage: null,
-    // main: ["https://steam-fec.s3.amazonaws.com/steam1/main-1-1.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-2.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-3.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-4.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-5.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-6.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-7.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-8.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-9.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-10.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-11.jpg"],
-    // thumb: ["https://steam-fec.s3.amazonaws.com/steam1/thumb-1-1.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-2.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-3.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-4.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-5.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-6.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-7.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-8.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-9.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-10.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-11.jpg"],
   })
 
   useEffect(async () => {
@@ -26,7 +22,11 @@ const Gallery = () => {
     let main = res.data[0].mainImages.map(item => item.main)
     let thumb = res.data[0].mainImages.map(item => item.thumb)
 
-    setState(() => {
+    // let main = ["https://steam-fec.s3.amazonaws.com/steam1/main-1-1.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-2.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-3.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-4.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-5.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-6.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-7.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-8.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-9.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-10.jpg", "https://steam-fec.s3.amazonaws.com/steam1/main-1-11.jpg"]
+
+    // let thumb = ["https://steam-fec.s3.amazonaws.com/steam1/thumb-1-1.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-2.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-3.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-4.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-5.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-6.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-7.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-8.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-9.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-10.jpg", "https://steam-fec.s3.amazonaws.com/steam1/thumb-1-11.jpg"]
+
+    await setState(() => {
       return {
         ...state,
         main,
@@ -71,15 +71,11 @@ const Gallery = () => {
 
   return (
     < div id="gallery" >
-      <div>
-        <a className="category" href="">All Games<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></a>
-        <a className="category" href="">Strategy Games<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></a>
-        <a className="category" href="">Age of Empires Franchise Franchise<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></a>
-        <a className="category" href="">Age of Empires II: Definitive Edition</a></div>
-      <h1 id="title">{state.title}</h1>
-      {/* Comm Hub */}
+      <Categories />
+      <h1 id="title" style={{ color: 'black' }}>{state.title}</h1>
       <HubButton />
       <br />
+
       {/* main images */}
       <div className="container">
         {<div className="mySlides" >
@@ -99,14 +95,7 @@ const Gallery = () => {
           })}
         </div>
       </div>
-
-      {/* Sign in message */}
-      <p style={{ fontSize: '11px', backgroundColor: 'rgba(0, 0, 0, 0.1)', padding: '20px', marginRight: '175px' }}>
-        <span id="signin" style={{ color: 'white' }} >Sign in</span>
-        <span style={{ color: '#bbbab3' }}>&nbsp;to add this item to your wishlist, follow it, or mark it as not interested</span>
-      </p>
-      <br />
-      <br />
+      <SignIn />
     </div >
   );
 };
---
+++ client/src/components/SignIn.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const SignIn = () => {
+  return (
+    <div>
+      <p style={{ fontSize: '11px', backgroundColor: 'rgba(0, 0, 0, 0.1)', padding: '20px', marginRight: '175px' }}>
+        <span id="signin" style={{ color: 'white' }} >Sign in</span>
+        <span style={{ color: '#bbbab3' }}>&nbsp;to add this item to your wishlist, follow it, or mark it as not interested</span>
+      </p>
+      <br />
+      <br />
+    </div>
+  )
+}
+
+export default SignIn;
