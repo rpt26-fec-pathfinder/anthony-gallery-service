ab6cdc86a175323287dae5b937a7413016b810c9
Anthony Sim <anthonyysim@gmail.com>
2021-03-23 09:38:43 -0700

    realigned components

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const AllCategories = styled.div`
+  margin-bottom: -15px;
+  margin-top: -25px;
+`
+
 const Category = styled.a`
   color: #9c9b93;
   font-size: 11px;
   text-decoration: none;
+  margin-bottom: 50px;
 `
 
 const Categories = () => {
   const [category, setCategory] = useState('')
 
   return (
-    <div >
+    <AllCategories >
       <Category href="">All Games<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></Category>
       <Category href="">Strategy Games<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></Category>
       <Category href="">Age of Empires Franchise Franchise<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></Category>
       <Category href="">Age of Empires II: Definitive Edition</Category>
-    </div >
+    </AllCategories >
   )
 }
 
