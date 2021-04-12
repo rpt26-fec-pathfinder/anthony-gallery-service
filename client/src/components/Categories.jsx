2d19bd75bdab3924377b36220e49b8415caf8490
Anthony Sim <anthonyysim@gmail.com>
2021-04-11 20:59:59 -0700

    spacing for title

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -13,6 +13,7 @@ const Categories = () => {
       <Category href="">Strategy Games<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></Category>
       <Category href="">Age of Empires Franchise Franchise<span>&nbsp;&nbsp;>&nbsp;&nbsp;</span></Category>
       <Category href="">Age of Empires II: Definitive Edition</Category>
+      <br />
     </AllCategories >
   )
 }
