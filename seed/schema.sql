d7fe8d0ce021621d05a15c674d8f60bef4260b85
Max Proano <proanom89@gmail.com>
2021-05-17 20:19:59 -0700

    Complete inserting records into mysql db

--- seed/schema.sql
+++ seed/schema.sql
@@ -2,16 +2,25 @@ DROP DATABASE IF EXISTS photoService;
 CREATE DATABASE photoService;
 USE photoService;
 
-DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS headerImages;
 
-CREATE TABLE photos (
+CREATE TABLE headerImages (
   id INT NOT NULL AUTO_INCREMENT,
   headerImage varchar(255),
-  mainImages text,
-  randomPage varchar(255),
   PRIMARY KEY (id)
-)
+);
+
+DROP TABLE IF EXISTS mainImages;
+
+CREATE TABLE mainImages (
+  id INT NOT NULL AUTO_INCREMENT,
+  main varchar(255),
+  thumb varchar(255),
+  headerImageId INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (headerImageId) REFERENCES headerImages(id)
+);
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < db/schema.sql
+ *    mysql -u root < seed/schema.sql
  *  to create the database and the tables.*/
--- seed/seed.sql
+++ seed/seed.sql
@@ -1,11 +1,21 @@
+SET profiling=1;
+
 USE photoService;
 
-LOAD DATA LOCAL INFILE '/Users/maxpro/Documents/hackreactor/senior/sdc/anthony-photo-gallery-service/seed/data/pages.csv'
-INTO TABLE photos
-FIELDS TERMINATED BY '\t'
+LOAD DATA LOCAL INFILE '/Users/maxpro/Documents/hackreactor/senior/sdc/anthony-photo-gallery-service/seed/data/headerImages.csv'
+INTO TABLE headerImages
+FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS (id, headerImage, mainImages, randomPage)
+(id, headerImage);
+
+LOAD DATA LOCAL INFILE '/Users/maxpro/Documents/hackreactor/senior/sdc/anthony-photo-gallery-service/seed/data/mainImages.csv'
+INTO TABLE mainImages
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+(main, thumb, headerImageId);
+
+SHOW PROFILES;
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < db/seed.sql
+ *    mysql -u root < seed/seed.sql
  *  to create the database and the tables.*/
