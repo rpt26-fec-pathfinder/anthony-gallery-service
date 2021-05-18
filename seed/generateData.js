472ddb20b8952b8579c3b5077f01c3739b8e0e34
Max Proano <proanom89@gmail.com>
2021-05-17 20:18:25 -0700

    Refactor generateCSV to use drain

--- seed/generateData.js
+++ seed/generateData.js
@@ -1,52 +1,32 @@
 const fs = require('fs');
 const path = require('path');
-const csvWriter = require('csv-write-stream');
-const writer = csvWriter({ separator: '\t' });
-const Page = require('./Page.js');
-const filePath = path.join(__dirname, '/data');
 
 const timer = (record, start) => {
   if (record % 100000 === 0) {
     const now = Date.now();
     const minutesElapsed = (((now - start) / 1000) / 60);
-    console.log(`created record #${record} - minutes elapsed: ${minutesElapsed}`);
+    console.log(`records left to create #${record} - minutes elapsed: ${minutesElapsed}`);
   }
 }
 
 const start = Date.now();
 
-writer.pipe(fs.createWriteStream(filePath + '/pages.csv'));
-
-module.exports.generateCSV = async () => {
-  for (let i = 1; i <= 1e7 ; i++) {
-    writer.write((new Page(i)));
-
-    try {
-      await new Promise(r => setImmediate(r));
-    } catch (err) {
-      console.log(err);
+module.exports.generateCSV = (writer, data, records, cb) => {
+  let i = 0;
+  write();
+  function write() {
+    let ok = true;
+    do {
+      i++;
+      if (i === records) {
+        writer.write(data(i), cb);
+      } else {
+        ok = writer.write(data(i));
+        timer(i, start);
+      }
+    } while (i < records && ok);
+    if (i < records) {
+      writer.once('drain', write);
     }
-
-    timer(i, start);
-}
-
-  writer.end();
-  console.log(`10M records created in ${(((Date.now() - start) / 1000) / 60)}`)
-  console.log('done');
-};
-
-const stream = fs.createWriteStream(filePath + '/pages.json');
-
-module.exports.generateJSON = async () => {
-  for (let i = 1; i <= 1e7; i++) {
-    if (!stream.write(JSON.stringify(new Page(i), null, 2) + '\n')) {
-      await new Promise(resolve => stream.once('drain', resolve));
-    }
-
-    timer(i, start);
   }
-
-  stream.end();
-  console.log(`10M records created in ${(((Date.now() - start) / 1000) / 60)}`)
-  console.log('Seeding complete');
 };
