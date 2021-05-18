43cad3a97efc3149cb3833529e37f6c341155b8b
Max Proano <proanom89@gmail.com>
2021-05-17 20:17:46 -0700

    Seperate headerImage and mainImage generation

--- seed/Page.js
+++ seed/Page.js
@@ -1,26 +1,14 @@
 const { randomPageSelector, generateNumUpTo } = require('./randomPageSelector');
 
-module.exports = class Page {
-  constructor(pageId) {
-    this.page = pageId;
-    this.headerImage = '';
-    this.mainImages = [];
-    this.randomPage = randomPageSelector();
+module.exports.createHeaderImageRecord = (id) => {
+  const randomPage = randomPageSelector();
+  return `${id},https://steam-fec.s3.amazonaws.com/steam${randomPage}/header-${randomPage}.jpg\n`;
+};
 
-    this.setHeaderImage();
-    this.setMainImages();
-  }
-
-  setHeaderImage() {
-    this.headerImage = `https://steam-fec.s3.amazonaws.com/steam${this.randomPage}/header-${this.randomPage}.jpg`;
-  }
-
-  setMainImages() {
-    for (let i = 1; i <= generateNumUpTo(8); i++) {
-      this.mainImages.push(JSON.stringify({
-        main: `https://steam-fec.s3.amazonaws.com/steam${this.randomPage}/main-${this.randomPage}-${i}.jpg`,
-        thumb: `https://steam-fec.s3.amazonaws.com/steam${this.randomPage}/thumb-${this.randomPage}-${i}.jpg`
-      }));
-    }
-  }
-}
+module.exports.createMainImageRecords = (id) => {
+  const randomPage = randomPageSelector();
+  return [...new Array(8)].map((currentelement, index) => {
+      return `https://steam-fec.s3.amazonaws.com/steam${randomPage}/main-${randomPage}-${index}.jpg,https://steam-fec.s3.amazonaws.com/steam${randomPage}/thumb-${randomPage}-${index}.jpg,${id}\n`
+    })
+    .join('');
+};
