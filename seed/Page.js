37cfced28f492659f385443a65e8e44f7c82293e
Max Proano <proanom89@gmail.com>
2021-05-21 18:08:11 -0700

    Complete generating data for couchdb

--- seed/Page.js
+++ seed/Page.js
@@ -12,3 +12,14 @@ module.exports.createMainImageRecords = (id) => {
     })
     .join('');
 };
+
+module.exports.createCouchRecord = (id) => {
+  const randomPage = randomPageSelector();
+  const mainImages = [...new Array(8)].map((currentelement, index) => {
+    return JSON.stringify({
+      main: `https://steam-fec.s3.amazonaws.com/steam${randomPage}/main-${randomPage}-${index}.jpg`,
+      thumb: `https://steam-fec.s3.amazonaws.com/steam${randomPage}/thumb-${randomPage}-${index}.jpg`
+    })
+  })
+  return `${id}\thttps://steam-fec.s3.amazonaws.com/steam${randomPage}/header-${randomPage}.jpg\t[${mainImages}]\n`
+}
--- seed/seedingScript.js
+++ seed/seedingScript.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const { generateCSV, generateJSON } = require('./generateData');
-const { createHeaderImageRecord, createMainImageRecords } = require('./Page.js');
+const { createHeaderImageRecord, createMainImageRecords, createCouchRecord } = require('./Page.js');
 const filePath = path.join(__dirname, '/data');
 
 try {
   const headerImagesStream = fs.createWriteStream(filePath + '/headerImages.csv');
   generateCSV(headerImagesStream, createHeaderImageRecord, 1e7, () => {
-    headerImagesStream.end()
-    console.log('Successfully wrote headerImage records to CSV')
+    headerImagesStream.end();
+    console.log('Successfully wrote headerImage records to CSV');
   });
 } catch (err) {
   if (err) {console.log(err)}
@@ -18,8 +18,19 @@ try {
   const mainImagesStream = fs.createWriteStream(filePath + '/mainImages.csv');
   generateCSV(mainImagesStream, createMainImageRecords, 1e7, () => {
     mainImagesStream.end();
-    console.log('Successfully wrote mainImage records to CSV')
+    console.log('Successfully wrote mainImage records to CSV');
   });
+} catch (err) {
+  if (err) {console.log(err)}
+}
+
+try {
+  const couchStream = fs.createWriteStream(filePath + '/couch.csv');
+  couchStream.write('_id\theaderImage\tmainImages\n');
+  generateCSV(couchStream, createCouchRecord, 1e7, () => {
+    couchStream.end();
+    console.log('Successfully wrote couch records to CSV');
+  })
 } catch (err) {
   if (err) {console.log(err)}
 }
