d2e37cc870630b3ac341b04b999d2ca440712811
Max Proano <proanom89@gmail.com>
2021-06-02 08:15:01 -0700

    Create couchdb update and delete controllers

--- server/controllers/couch.js
+++ server/controllers/couch.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const axios = require('axios');
+const axios = require('axios').default;
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || '5984';
 const userName = process.env.COUCH_USER;
@@ -16,31 +16,52 @@ const runAsyncWrapper = (callback) => {
 // create image
 module.exports.createGallery = runAsyncWrapper(async(req, res) => {
   const dbInfo = await axios.get(couchURL);
-  let docCount = dbInfo.data.doc_count;
-  docCount++;
+  let { doc_count, doc_del_count} = dbInfo.data;
+  const id = doc_count + doc_del_count + 1;
 
-  const headerImage = req.body.headerImage;
-  const mainImages = req.body.mainImages;
-  const result = await axios.put(couchURL + `/${docCount}`, {
+  const { headerImage, mainImages } = req.body;
+  const result = await axios.put(couchURL + `/${id}`, {
     headerImage,
     mainImages
   });
+
   res.status(201).json(result.data);
-})
+});
 
 // get image
 module.exports.getGallery = runAsyncWrapper(async(req, res) => {
   const _id = req.params.page;
   const result = await axios.get(couchURL + `/${_id}`);
-  res.status(200).json(result.data);
-})
+  res.status(200).json([result.data]);
+});
 
 // update image
-module.exports.updateGallery = () => {
+module.exports.updateGallery = runAsyncWrapper(async(req, res) => {
+  const _id = req.params.page;
+  const docInfo = await axios.get(couchURL + `/${_id}`);
+  const _rev = docInfo.data._rev;
 
-};
+  const { headerImage, mainImages } = req.body;
+  const result = await axios.put(couchURL + `/${_id}`, {
+    _rev,
+    headerImage,
+    mainImages
+  });
+
+  res.json(result.data);
+});
 
 // delete image
-module.exports.deleteGallery = () => {
+module.exports.deleteGallery = runAsyncWrapper(async(req, res) => {
+  const _id = req.params.page;
+  const docInfo = await axios.get(couchURL + `/${_id}`);
+  const _rev = docInfo.data._rev;
 
-};
+  const result = await axios.delete(couchURL + `/${_id}`, {
+    params: {
+      rev: _rev
+    }
+  });
+
+  res.json(result.data);
+});
