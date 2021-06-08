74e4ecc426d904ccda5e61b99ad9f27829e4347b
Max Proano <proanom89@gmail.com>
2021-06-07 19:03:48 -0700

    Remove request for meta data

--- client/src/components/Name.jsx
+++ client/src/components/Name.jsx
@@ -6,17 +6,17 @@ import axios from 'axios';
 const Name = () => {
   const [title, setTitle] = useState('');
 
-  useEffect(async () => {
-    try {
-      const metaRes = await axios.get(`/api/product${window.location.pathname}`);
+  // useEffect(async () => {
+  //   try {
+  //     const metaRes = await axios.get(`/api/product${window.location.pathname}`);
 
-      metaRes.data.message
-        ? await setTitle('404 Error, title not found!')
-        : await setTitle(metaRes.data.name);
-    } catch (err) {
-      console.error(err)
-    }
-  }, [])
+  //     metaRes.data.message
+  //       ? await setTitle('404 Error, title not found!')
+  //       : await setTitle(metaRes.data.name);
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+  // }, [])
 
   return (
     <Title>{title}</Title>
